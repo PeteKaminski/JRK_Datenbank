58c0dd2cf058c0ec596c595cb979ccfcdbf3f483
rensky <rensky.g@googlemail.com>
2012-06-11 23:06:16 +0200

    post array test

--- JRKDatenbank/application/views/form/erfolg.php
+++ JRKDatenbank/application/views/form/erfolg.php
@@ -6,7 +6,22 @@
 
 <h3>Your form was successfully submitted!</h3>
 
-<p><?php echo anchor('form', 'Try it again!'); ?></p>
+<p><?php 
+
+if ( ! isset($_POST['Formular']))
+ {
+     echo 'ist gesetzt';
+ }
+ else
+ {
+     echo 'nicht gesetzt';
+ }
+
+echo "<pre>";
+print_r($_POST);
+echo "</pre>";
+
+echo anchor('form', 'Try it again!'); ?></p>
 
 </body>
 </html>
