31cb6a6c4695a2ebde2a64a16078e362cb01186e
larswillrich <larswillrich@googlemail.com>
2012-06-15 17:07:30 +0200

    tabellenform in veranstaltungen

--- JRKDatenbank/application/controllers/main.php
+++ JRKDatenbank/application/controllers/main.php
@@ -16,7 +16,7 @@ class main extends CI_Controller {
 	}
 	
 	private function isSessionValid(){
-		if ((now() - $this->session->userdata('last_activity')) < 60 ) return 1;
+		if ((now() - $this->session->userdata('last_activity')) < 60*5 ) return 1;
 		return 0;
 	}
 	
