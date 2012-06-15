4939643c4b25bdd0a7362640d3b8e8327b87d848
larswillrich <larswillrich@googlemail.com>
2012-06-15 13:20:14 +0200

    login form

--- JRKDatenbank/application/views/form/login.php
+++ JRKDatenbank/application/views/form/login.php
@@ -1,5 +1,15 @@
+<h1>Anmeldung in die JRK Datenbank.</h1>
+
 <?php	// Hier entsteht das Formular zum einloggen in die Mitgliederverwaltung
 	   	// Eine Sessin ID wird danach generiert um damit weiter Arbeiten zu können.
+$this->load->helper('form');
+
+echo form_open('main/index');
+echo "Testlogin mit</br> User: test</br>";
+echo "Passwort: test";
+echo form_input('username', 'User');
+echo form_password('username_pw', '');
+echo form_submit('login', 'Login');
+
 ?>
 
-<h1>Anmeldung in die JRK Datenbank.</h1>
